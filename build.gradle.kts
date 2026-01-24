e72f32005315d9657bb830395fc968618c22df9c
matquant14 <matquant14@users.noreply.github.com>
2026-01-24 16:55:34 -0600

    Remove `pluginUntilBuild` property and add placeholders for `platformBundledPlugins` and `platformBundledModules`

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,6 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
 
